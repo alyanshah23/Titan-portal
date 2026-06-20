1ab85a8c31a4e64fc743498fb44f6fc1b497f92f
alyanshah23 <alyanshah2378@gmail.com>
2026-06-20 11:08:04 -0700

    added routing

---
+++ src/My Components/Login.jsx
@@ -0,0 +1,14 @@
+function Login() {
+    return (
+        <>
+           <>
+           <h1>login page</h1>
+           <h1> page</h1>
+           <h1> page</h1>
+
+
+           </>
+        </>
+        )
+}
+export default Login 
