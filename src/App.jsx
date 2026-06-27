b07a12b0adc7971088000d37e0ce68ababdc6992
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-06-27 18:40:27 +0500

    details page started

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 
 import Login from "./My Components/Login"
-
+import details from "./My Components/details"
 function App() {
-    return (
-        // <Routes>
-        //     <Route path="/" element={<Navigate to="/Login" />} />
-        //     <Route path="/login" element={<Login />} />
+    return (<>
+        {/* <Routes>
+             <Route path="/" element={<Navigate to="/Login" />} />
+             <Route path="/login" element={<Login />} />
 
-        // </Routes>)
-    <Login/>)
+         </Routes>) */}
+    <Login/>
+    <details/>
+</>)
 }
 export default App
---
+++ src/My Components/details.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/lo-go.png"
+function details(){
+
+    return(<>
+    <div>
+        <div className="logo-container">
+            <img src={logo} alt="" />
+        </div>
+    </div>
+    
+    </>)
+}
+export default details;
